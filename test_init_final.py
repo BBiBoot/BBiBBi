cf999533142cf4dff1d1832264ffcb311412bf95
BBiBoot <57780779+BBiBoot@users.noreply.github.com>
2020-03-13 09:38:51 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -256,8 +256,8 @@ def init():
 			endTime = endTime + datetime.timedelta(days=int(basicSetting[13]))
 
 	### 채널 고정###
-	basicSetting[6] = int('656356269480869899') #보이스채널ID
-	basicSetting[7] = int('656356771182673942') #택스트채널ID
+	basicSetting[6] = int('575915268174381058') #보이스채널ID
+	basicSetting[7] = int('686459920807100447') #택스트채널ID
 	
 	bossNum = int(len(boss_inputData)/5)
 
